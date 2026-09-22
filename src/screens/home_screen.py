7422e5c5edc5a7760af245bb2e1d55d24cd0259a
Kayak Crasher <unlimiteddata247@gmail.com>
2026-09-22 15:34:30 -0500

    Add HomeScreen class with properties and spin method

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -1,1 +1,11 @@
+from kivy.properties import BooleanProperty, StringProperty
+from kivy.uix.screenmanager import Screen
 
+
+class HomeScreen(Screen):
+    current_recipe = StringProperty("")
+    is_spinning = BooleanProperty(False)
+
+    def spin(self):
+        # Implemented in the next step
+        pass
